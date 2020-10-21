6ba24caf3538d11de9b6d58afcb4c4aa697430b6
Esubalew Dessalegn <kemahu1@outlook.com>
2020-10-21 11:17:37 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello clever Programmers, let's build the Amazon Store
+      <span role="img" aria-label="sheep">😓😍</span>
+       </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB6KH8H-GQdVRNgo9NQ8EGpZvWhb2kN69c",
+    authDomain: "challenge-7d965.firebaseapp.com",
+    databaseURL: "https://challenge-7d965.firebaseio.com",
+    projectId: "challenge-7d965",
+    storageBucket: "challenge-7d965.appspot.com",
+    messagingSenderId: "720823475076",
+    appId: "1:720823475076:web:11a1b855b2f77edf4f1bd1",
+    measurementId: "G-RNG4E36HHY"
+  };
