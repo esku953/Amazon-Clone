b39bc9d026606b6c432c4f2428b082eac95e1655
Esubalew Dessalegn <kemahu1@outlook.com>
2020-10-21 13:32:31 -0400

    added files

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,18 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyB6KH8H-GQdVRNgo9NQ8EGpZvWhb2kN69c",
-    authDomain: "challenge-7d965.firebaseapp.com",
-    databaseURL: "https://challenge-7d965.firebaseio.com",
-    projectId: "challenge-7d965",
-    storageBucket: "challenge-7d965.appspot.com",
-    messagingSenderId: "720823475076",
-    appId: "1:720823475076:web:11a1b855b2f77edf4f1bd1",
-    measurementId: "G-RNG4E36HHY"
-  };
+  apiKey: "AIzaSyCcPSKlYtpdzBoAC8soeSmIARMzVKzrf5I",
+  authDomain: "challenge-4b2b2.firebaseapp.com",
+  databaseURL: "https://challenge-4b2b2.firebaseio.com",
+  projectId: "challenge-4b2b2",
+  storageBucket: "challenge-4b2b2.appspot.com",
+  messagingSenderId: "962418448875",
+  appId: "1:962418448875:web:f6cce5eeaf819481f661ae",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
